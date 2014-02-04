d12b932bab42df5ba6d129b0ee38606fe59d1bb2
ntkyger <nicholas.kyger@gmail.com>
2014-02-03 19:58:09 -0500

    adding unity files

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
+var path = require('path');
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'Unity'))); 
 
 app.get('/', function(req, res) {
   res.send('Hello Heroku World!');
