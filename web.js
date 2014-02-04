9479d2ecf6f06fb8016a030b1c7ceb6b87a2cdbe
ntkyger <nicholas.kyger@gmail.com>
2014-02-03 21:59:37 -0500

    adding crossdomain.xml

--- web.js
+++ web.js
@@ -2,14 +2,26 @@ var path = require('path');
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var asyncblock = require("asyncblock");
 
 app.use(logfmt.requestLogger());
 
-// Serve static files
+// Serve static Unity files
 app.use(express.static(path.join(__dirname, 'Unity'))); 
 
-app.get('/', function(req, res) {
-  res.send('Hello Heroku World!');
+app.get('/', function(request, response) {
+
+	asyncblock(function(flow) {
+		var dir = spawn('ls');
+		
+		var result = flow.wait();
+		console.log(result);
+
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Spawned child process:" + result);
+		response.end();
+	});
+  //res.send('Hello Heroku World!');
 });
 
 var port = Number(process.env.PORT || 5000);
